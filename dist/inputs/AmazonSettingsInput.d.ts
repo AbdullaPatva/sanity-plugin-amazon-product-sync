bc5eb93b4e80f34dc89437eb8c941a56a7b87b77
Abdulla Patva <abdulla.patva@multidots.com>
2025-08-08 20:14:58 +0530

    Cleanup: remove unnecessary files - redundant AmazonSettingsInput, dev docs, rebuild dist

--- dist/inputs/AmazonSettingsInput.d.ts
+++
@@ -1,4 +0,0 @@
-import type { FormInputProps } from 'sanity';
-type Props = FormInputProps;
-export declare function AmazonSettingsInput(props: Props): import("react/jsx-runtime").JSX.Element;
-export {};
